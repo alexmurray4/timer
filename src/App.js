c0847769543387e2aa306771b9270b04e241576a
Alex Murray <alexmurray@Alexs-Computer.local>
2019-08-30 17:41:33 +1200

    adding button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Clock from './Clock.js';
 
+function handleClick(e) {
+    e.preventDefault();
+    console.log('The link was clicked.');
+}
+
 function App() {
   return (
     <div className="App">
         <Clock />
+        <a href="#" onClick={handleClick}>
+          Click me
+        </a>
     </div>
   );
 }
